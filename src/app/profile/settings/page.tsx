611f530f843ec65c04e0dfcc6479f2b3945315d6
zeyar myo khant zeyar <zeyarzeyarmyokhant@gmail.com>
2025-08-29 09:59:57 +0000

    setting page ကပေါ်မလာဘူးဖြသ်နေတယ်ဗျာ့

--- src/app/profile/settings/page.tsx
+++ src/app/profile/settings/page.tsx
@@ -1,4 +1,6 @@
 
+"use client";
+
 import { Button } from "@/components/ui/button";
 import { ArrowLeft, User, Shield, Globe, Ban, ChevronRight, LogOut } from "lucide-react";
 import Link from "next/link";
