20fc6339dfd33fe3d7e8861ebc331709e5218582
zeyar myo khant zeyar <zeyarzeyarmyokhant@gmail.com>
2025-09-28 03:11:19 +0000

    Error: Error fetching friends: {}     at createConsoleError (https://600

--- src/app/ai-tool/typing-battle/page.tsx
+++ src/app/ai-tool/typing-battle/page.tsx
@@ -43,7 +43,7 @@ export default function TypingBattleSetupPage() {
             
             const { data: followingData, error: followingError } = await supabase
                 .from('followers')
-                .select('profiles!followers_user_id_fkey(*, win_streak_3, win_streak_10)')
+                .select('profiles!user_id(*, win_streak_3, win_streak_10)')
                 .eq('follower_id', user.id);
 
             if (followingError) {
