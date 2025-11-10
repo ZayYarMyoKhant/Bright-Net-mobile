05c0f0bf7c38b4df1487a7651483266337407cd0
zeyar myo khant zeyar <zeyarzeyarmyokhant@gmail.com>
2025-11-10 06:29:03 +0000

    feat: Completed Capacitor setup for Android build

---
+++ capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'com.Bright-Net.app',
+  appName: 'npx cap add android',
+  webDir: 'public'
+};
+
+export default config;
