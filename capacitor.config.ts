6d160b5a4d2d1177579a3338fb67759fc6c926b4
zeyar myo khant zeyar <zeyarzeyarmyokhant@gmail.com>
2025-11-19 16:58:44 +0000

    ဟာမဟုတ်ဘူးလေ ပြန်မည့်နဲ့လေ အဲ့ output exportကိုပြန်ထည့်ရင် generteStatic

--- capacitor.config.ts
+++ capacitor.config.ts
@@ -3,7 +3,11 @@ import type { CapacitorConfig } from '@capacitor/cli';
 const config: CapacitorConfig = {
   appId: 'com.brightnet.app',
   appName: 'Bright-Net',
-  webDir: 'out'
+  webDir: 'out',
+  server: {
+    url: 'http://localhost:3000',
+    cleartext: true
+  }
 };
 
 export default config;
